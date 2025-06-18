40b4a8efc42cb58e764baffd19a5a5b83eedb852
miya <tukiya8@yahoo.co.jp>
2025-06-19 04:30:03 +0900

    管理画面,コントローラ,ルーティング

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/weight_logs', [AdminController::class, 'index']);
