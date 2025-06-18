bdbd94885b66fc48248ac7cda7a2b0ffb678b17b
miya <tukiya8@yahoo.co.jp>
2025-06-19 05:03:27 +0900

    マイグレーション、コントローラ

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Weight_target;
+use App\Models\Weight_log;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin', compact('weight_target', 'weight_logs'));
+        $weight_targets = Weight_target::all();
+        $weight_logs = weight_log::all();
+        return view('admin', compact('weight_targets', 'weight_logs'));
     }
 }
