7d86cee0a71040043924703361cdc179fad79c6e
miya <tukiya8@yahoo.co.jp>
2025-06-19 10:00:06 +0900

    コントローラ,検索

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Weight_target;
 use App\Models\Weight_log;
+use Carbon\Carbon;
 
 class AdminController extends Controller
 {
@@ -19,10 +20,15 @@ class AdminController extends Controller
 
 public function search(Request $request)
     {
-        $weight_logs = $request->input('from');
-        $weight_logs = $request->input('until');
+        $query = Weight_log::query();
 
-        $results = Model::whereBetween('date', [$Weight_logs])->get();
+        if ($request->filled('start_date') && $request->filled('end_date')) {
+        $startDate = Carbon::parse($request->input('start_date'));
+        $endDate = Carbon::parse($request->input('end_date'));
+        $query->whereBetween('date', [$startDate, $endDate]);
+    }
+    
+        $results = $query->get();
 
         return view('admin', compact('weight_logs'));
     }
