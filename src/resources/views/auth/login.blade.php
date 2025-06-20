8df5f6ed5a7e42cf4e47351de5b382018241616f
miya <tukiya8@yahoo.co.jp>
2025-06-20 19:55:40 +0900

    CSS

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -14,21 +14,12 @@
     <main>
         <div class="login-form__content">
             <div class="login-form__heading">
-                <h1>PiGLy</h1>
-                <h2>ログイン</h2>
+                <div class="login-form_title1">PiGLy</div>
+                <div class="login-form_title2">ログイン</div>
             </div>
             <div class="login-form__inner">
                 <form class="login-form__form" action="/login" method="post">
                     @csrf
-                    <div class="login-form__group">
-                        <span class="login-form__label">お名前</span>
-                        <input class="login-form__input" type="text" name="name" value="{{ old('name') }}" />
-                        <div class="register-form__error-message">
-                            @error('name')
-                            {{ $message }}
-                            @enderror
-                        </div>
-                    </div>
                     <div class="login-form__group">
                         <span class="login-form__label">メールアドレス</span>
                         <input class="login-form__input" type="email" name="email" value="{{ old('email') }}" />
@@ -50,8 +41,8 @@
                     <button class="login-form__btn btn" type="submit">ログイン</button>
                 </form>
             </div>
+            <a class="register__link" href="/register/step1">アカウント作成はこちら</a>
         </div>
-        <a class="register__link" href="/register/step1">アカウント作成はこちら</a>
     </main>
 </body>
 
