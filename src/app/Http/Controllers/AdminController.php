2742d749af4c10849c4c6acccfcd421a85d759ee
miya <tukiya8@yahoo.co.jp>
2025-06-23 22:10:31 +0900

    日付検索　修正

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -22,11 +22,16 @@ class AdminController extends Controller
     {
         $query = Weight_log::query();
 
-        if ($request->filled('start_date') && $request->filled('end_date')) {
-        $startDate = Carbon::parse($request->input('start_date'));
-        $endDate = Carbon::parse($request->input('end_date'));
-        $query->whereBetween('date', [$startDate, $endDate]);
-    }
+        if ($request->filled('start_date')) {
+            $startDate = Carbon::parse($request->input('start_date'));
+            $query->where('date', '>=', $startDate);
+        }
+
+        if ($request->filled('end_date')) {
+            $endDate = Carbon::parse($request->input('end_date'));
+            $query->where('date', '<=', $endDate->endOfDay());
+        }
+
 
         $results = $query->get();
 
