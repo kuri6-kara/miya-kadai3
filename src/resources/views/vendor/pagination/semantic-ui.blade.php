91520becc4ca73c637963aaf7f29c03a1f265cfd
miya <tukiya8@yahoo.co.jp>
2025-08-05 22:28:51 +0900

    CSS修正

--- src/resources/views/vendor/pagination/semantic-ui.blade.php
+++ src/resources/views/vendor/pagination/semantic-ui.blade.php
@@ -1,36 +1,36 @@
 @if ($paginator->hasPages())
-    <div class="ui pagination menu" role="navigation">
-        {{-- Previous Page Link --}}
-        @if ($paginator->onFirstPage())
-            <a class="icon item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')"> <i class="left chevron icon"></i> </a>
-        @else
-            <a class="icon item" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')"> <i class="left chevron icon"></i> </a>
-        @endif
+<ul class="pagination">
+    {{-- Previous Page Link --}}
+    @if ($paginator->onFirstPage())
+    <li class="disabled" aria-disabled="true"><span>&lt;</span></li> {{-- ここを「<」に変更 --}}
+    @else
+    <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">&lt;</a></li> {{-- ここを「<」に変更 --}}
+    @endif
 
-        {{-- Pagination Elements --}}
-        @foreach ($elements as $element)
-            {{-- "Three Dots" Separator --}}
-            @if (is_string($element))
-                <a class="icon item disabled" aria-disabled="true">{{ $element }}</a>
-            @endif
+    {{-- Pagination Elements --}}
+    @foreach ($elements as $element)
+    {{-- "Three Dots" Separator --}}
+    @if (is_string($element))
+    <li class="disabled" aria-disabled="true"><span>{{ $element }}</span></li>
+    @endif
 
-            {{-- Array Of Links --}}
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <a class="item active" href="{{ $url }}" aria-current="page">{{ $page }}</a>
-                    @else
-                        <a class="item" href="{{ $url }}">{{ $page }}</a>
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
+    {{-- Array Of Links --}}
+    @if (is_array($element))
+    @foreach ($element as $page => $url)
+    @if ($page == $paginator->currentPage())
+    <li class="active" aria-current="page"><span>{{ $page }}</span></li>
+    @else
+    <li><a href="{{ $url }}">{{ $page }}</a></li>
+    @endif
+    @endforeach
+    @endif
+    @endforeach
 
-        {{-- Next Page Link --}}
-        @if ($paginator->hasMorePages())
-            <a class="icon item" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')"> <i class="right chevron icon"></i> </a>
-        @else
-            <a class="icon item disabled" aria-disabled="true" aria-label="@lang('pagination.next')"> <i class="right chevron icon"></i> </a>
-        @endif
-    </div>
+    {{-- Next Page Link --}}
+    @if ($paginator->hasMorePages())
+    <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">&gt;</a></li> {{-- ここを「>」に変更 --}}
+    @else
+    <li class="disabled" aria-disabled="true"><span>&gt;</span></li> {{-- ここを「>」に変更 --}}
+    @endif
+</ul>
 @endif
