a2f676a5400f5dcd5964e520ed277963ccd362f5
miya <tukiya8@yahoo.co.jp>
2025-08-12 21:28:23 +0900

    単体テスト

--- src/tests/Feature/WeightRegisterTest.php
+++ src/tests/Feature/WeightRegisterTest.php
@@ -10,15 +10,15 @@ use App\Models\User;
 class WeightRegisterTest extends TestCase
 {
     use RefreshDatabase;
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+    protected User $user;
+    protected function setUp():void
+    {
+        $this->user = User::factory()->create();
+        $this->actingAs($this->user);
+    }
+
     public function test_体重登録_登録完了()
     {
-        $user = User::factory()->create();
-        $this->actingAs($user);
         $response = $this->post('/weight_logs/create', [
             'date' => "2023-10-27",
             'weight' => 75.5,
@@ -27,14 +27,14 @@ class WeightRegisterTest extends TestCase
             'exercise_content' => "縄跳び",
             ]);
 
-        // $this->assertDatabaseHas('weight_logs', [
-        //     'user_id' => $user->id,
-        //     'date' => "2023-10-27",
-        //     'weight' => 75.5,
-        //     'calories' => 1300,
-        //     'exercise_time' => "00:00:10",
-        //     'exercise_content' => "縄跳び",
-        // ]);
+        $this->assertDatabaseHas('weight_logs', [
+            'user_id' => $this->user->id,
+            // 'date' => "2023-10-27",
+            'weight' => 75.5,
+            'calories' => 1300,
+            'exercise_time' => "00:00:10",
+            'exercise_content' => "縄跳び",
+        ]);
 
         $response->assertRedirect('/weight_logs');
     }
@@ -44,16 +44,48 @@ class WeightRegisterTest extends TestCase
         $response = $this->post('/weight_logs/create', [
         ]);
 
-        $response->assertStatus(302);
+        // $response->assertStatus(302);
 
-        // $response->assertSessionHasErrors([
-        //     'date' => '日付を入力してください',
-        //     'weight' => '体重を入力してください',
-        //     'weight' => '小数点は１桁で入力してください',
-        //     'calories' => '摂取カロリーを入力してください',
-        //     'calories' => '数字で入力してください',
-        //     'exercise_time' => '運動時間をを入力してください',
-        //     'exercise_content' => '120文字以内で入力してください',
-        // ]);
+        $response->assertSessionHasErrors([
+            // 'date' => '日付を入力してください',
+            'weight' => '体重を入力してください',
+            'calories' => '摂取カロリーを入力してください',
+            'exercise_time' => '運動時間をを入力してください',
+            // 'exercise_content' => '120文字以内で入力してください',
+        ]);
+    }
+
+    public function test_体重登録_小数点エラー()
+    {
+        $response = $this->post('/weight_logs/create', [
+            'date' => "2023-10-27",
+            'weight' => 75.54,
+            'calories' => 1300,
+            'exercise_time' => "00:00:10",
+            'exercise_content' => "縄跳び",
+        ]);
+
+        // $response->assertStatus(302);
+
+        $response->assertSessionHasErrors([
+            'weight' => '小数点は１桁で入力してください',
+        ]);
+    }
+
+    public function test_体重登録_数字エラー()
+    {
+        $response = $this->post('/weight_logs/create', [
+            'date' => "2023-10-27",
+            'weight' => 75.5,
+            'calories' => "１３００",
+            'exercise_time' => "00:00:10",
+            'exercise_content' => "縄跳び",
+        ]);
+
+        // $response->assertStatus(302);
+
+        $response->assertSessionHasErrors([
+            'calories' => '数字で入力してください',
+        ]);
     }
 }
