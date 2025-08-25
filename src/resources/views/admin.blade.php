9b3468c7d54d5faa02326f03c62a72a165951e0a
miya <tukiya8@yahoo.co.jp>
2025-08-25 23:26:51 +0900

    CSS修正

--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -44,6 +44,7 @@
             </div>
         </div>
 
+
         <div class="content">
             <div class="form1">
                 <form action="/weight_logs" method="GET" class="search-form" novalidate>
@@ -79,70 +80,81 @@
                         <form action="/weight_logs/create" method="POST" novalidate>
                             @csrf
                             <div class="modal_form-item">
-                                <div class="form-title">
-                                    <span class="form--item">日付</span>
-                                    <span class="form--required">必須</span>
-                                </div>
-                                <div class="form-content">
-                                    <input class="modal-form__date" type="date" name="date" value="{{ old('date', date('Y-m-d')) }}">
-                                </div>
-                                <div class="form__error">
-                                    @error('date')
-                                    {{ $message }}
-                                    @enderror
-                                </div>
-
-                                <div class="form-title">
-                                    <span class="form--item">体重</span>
-                                    <span class="form--required">必須</span>
-                                </div>
-                                <div class="form-content">
-                                    <input type="number" name="weight" placeholder="50.0" value="{{ old('weight') }}" step="0.1" />
-                                    <p>kg</p>
-                                </div>
-                                <div class="form__error">
-                                    @error('weight')
-                                    {{ $message }}
-                                    @enderror
+                                <div class="modal_title">Weight Logを追加</div>
+                                <div class="modal_content1">
+                                    <div class="form-title">
+                                        <span class="form--item">日付</span>
+                                        <span class="form--required">必須</span>
+                                    </div>
+                                    <div class="form-content">
+                                        <input class="modal-form__date" type="date" name="date" value="{{ old('date', date('Y-m-d')) }}">
+                                    </div>
+                                    <div class="form__error">
+                                        @error('date')
+                                        {{ $message }}
+                                        @enderror
+                                    </div>
                                 </div>
 
-                                <div class="form-title">
-                                    <span class="form--item">摂取カロリー</span>
-                                    <span class="form--required">必須</span>
-                                </div>
-                                <div class="form-content">
-                                    <input type="number" name="calories" placeholder="1200" value="{{ old('calories') }}" />
-                                    <p>cal</p>
-                                </div>
-                                <div class="form__error">
-                                    @error('calories')
-                                    {{ $message }}
-                                    @enderror
-                                </div>
-
-                                <div class="form-title">
-                                    <span class="form--item">運動時間</span>
-                                    <span class="form--required">必須</span>
-                                </div>
-                                <div class="form-content">
-                                    <input type="time" name="exercise_time" placeholder="00:00" value="{{ old('exercise_time') ?? '00:00' }}" />
-                                </div>
-                                <div class="form__error">
-                                    @error('exercise_time')
-                                    {{ $message }}
-                                    @enderror
+                                <div class="modal_content2">
+                                    <div class="form-title">
+                                        <span class="form--item">体重</span>
+                                        <span class="form--required">必須</span>
+                                    </div>
+                                    <div class="form-content">
+                                        <input type="number" name="weight" placeholder="50.0" value="{{ old('weight') }}" step="0.1" />
+                                        <p>kg</p>
+                                    </div>
+                                    <div class="form__error">
+                                        @error('weight')
+                                        {{ $message }}
+                                        @enderror
+                                    </div>
                                 </div>
 
-                                <div class="form-title">
-                                    <span class="form--item">運動内容</span>
+                                <div class="modal_content3">
+                                    <div class="form-title">
+                                        <span class="form--item">摂取カロリー</span>
+                                        <span class="form--required">必須</span>
+                                    </div>
+                                    <div class="form-content">
+                                        <input type="number" name="calories" placeholder="1200" value="{{ old('calories') }}" />
+                                        <p>cal</p>
+                                    </div>
+                                    <div class="form__error">
+                                        @error('calories')
+                                        {{ $message }}
+                                        @enderror
+                                    </div>
                                 </div>
-                                <div class="form-content">
-                                    <input type="text" name="exercise_content" placeholder="運動内容を追加" value="{{ old('exercise_content') }}" />
+
+                                <div class="modal_content4">
+                                    <div class="form-title">
+                                        <span class="form--item">運動時間</span>
+                                        <span class="form--required">必須</span>
+                                    </div>
+                                    <div class="form-content">
+                                        <input type="time" name="exercise_time" placeholder="00:00" value="{{ old('exercise_time') ?? '00:00' }}" />
+                                    </div>
+                                    <div class="form__error">
+                                        @error('exercise_time')
+                                        {{ $message }}
+                                        @enderror
+                                    </div>
                                 </div>
-                                <div class="form__error">
-                                    @error('exercise_content')
-                                    {{ $message }}
-                                    @enderror
+
+                                <div class="modal_content5">
+                                    <div class="form-title">
+                                        <span class="form--item">運動内容</span>
+                                    </div>
+                                    <div class="form-content">
+                                        <input type="text" name="exercise_content" placeholder="運動内容を追加" value="{{ old('exercise_content') }}" />
+                                    </div>
+                                    <div class="form__error">
+                                        @error('exercise_content')
+                                        {{ $message }}
+                                        @enderror
+                                    </div>
                                 </div>
 
                                 <div class="return-form__button">
@@ -157,48 +169,49 @@
                 </div>
                 <script src="{{ asset('js/modal.js') }}"></script>
             </div>
-        </div>
 
-        {{-- 検索結果の表示（件数と条件） --}}
-        @if (isset($search_performed) && $search_performed)
-        <div class="search-results-summary">
-            @if ($start_date && $end_date)
-            <p>{{ $start_date }} 〜 {{ $end_date }} の検索結果 {{ $search_count }}件</p>
-            @elseif ($start_date)
-            <p>{{ $start_date }} 以降の検索結果 {{ $search_count }}件</p>
-            @elseif ($end_date)
-            <p>{{ $end_date }} までの検索結果 {{ $search_count }}件</p>
-            @else
-            <p>全期間の検索結果 {{ $search_count }}件</p>
+
+            {{-- 検索結果の表示（件数と条件） --}}
+            @if (isset($search_performed) && $search_performed)
+            <div class="search-results-summary">
+                @if ($start_date && $end_date)
+                <p>{{ $start_date }} 〜 {{ $end_date }} の検索結果 {{ $search_count }}件</p>
+                @elseif ($start_date)
+                <p>{{ $start_date }} 以降の検索結果 {{ $search_count }}件</p>
+                @elseif ($end_date)
+                <p>{{ $end_date }} までの検索結果 {{ $search_count }}件</p>
+                @else
+                <p>全期間の検索結果 {{ $search_count }}件</p>
+                @endif
+            </div>
             @endif
-        </div>
-        @endif
 
-        <div class="form2">
-            <table class="form-content">
-                <tr class="admin__row">
-                    <th class="admin__label">日付</th>
-                    <th class="admin__label">体重</th>
-                    <th class="admin__label">食事摂取カロリー</th>
-                    <th class="admin__label">運動時間</th>
-                    <th class="admin__label"></th>
-                </tr>
-                @foreach($weight_logs as $weight_log)
-                <tr class="admin__row">
-                    <td class="admin__data">{{$weight_log->date}}</td>
-                    <td class="admin__data">{{$weight_log->weight}}</td>
-                    <td class="admin__data">{{$weight_log->calories}}</td>
-                    <td class="admin__data">{{$weight_log->exercise_time}}</td>
-                    <td class="admin__data">
-                        <a href="/weight_logs/{{$weight_log->id}}">
-                            <img src="{{ asset('img/Group.png') }}">
-                        </a>
-                    </td>
-                </tr>
-                @endforeach
-            </table>
-            <div class="pagination-content">
-                {{ $weight_logs->links('vendor.pagination.semantic-ui') }}
+            <div class="form2">
+                <table class="form-content">
+                    <tr class="admin__row">
+                        <th class="admin__label">日付</th>
+                        <th class="admin__label">体重</th>
+                        <th class="admin__label">食事摂取カロリー</th>
+                        <th class="admin__label">運動時間</th>
+                        <th class="admin__label"></th>
+                    </tr>
+                    @foreach($weight_logs as $weight_log)
+                    <tr class="admin__row">
+                        <td class="admin__data">{{$weight_log->date}}</td>
+                        <td class="admin__data">{{$weight_log->weight}}</td>
+                        <td class="admin__data">{{$weight_log->calories}}</td>
+                        <td class="admin__data">{{$weight_log->exercise_time}}</td>
+                        <td class="admin__data">
+                            <a href="/weight_logs/{{$weight_log->id}}">
+                                <img src="{{ asset('img/Group.png') }}">
+                            </a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </table>
+                <div class="pagination-content">
+                    {{ $weight_logs->links('vendor.pagination.semantic-ui') }}
+                </div>
             </div>
         </div>
     </div>
